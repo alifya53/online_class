9afb419b31d12c060489c8abde71dbaa5cf55e18
alifya53 <alifyahussian53@gmail.com>
2020-07-12 21:44:07 +0500

    sunday class

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main(){
+    // let y = "Hello Pakistan!";
+    let mut x = 5;
+    let z = 8;
+    let sum = x + z;
+    println!("The sum is = {}",sum);
+    println!("The value of x ={}",x);
+    // println!("The value of y is : {}",y);
+
+     x = 10;
+    let sum2 = x + z;
+    println!("The value of x ={}",x);
+    println!("The sum is = {}",sum2);
+
+}
+
